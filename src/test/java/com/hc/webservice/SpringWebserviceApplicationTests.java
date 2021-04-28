fc5da3f51387a4b62a567470b1c7f43ae11ed402
hcjo1223 <whghcjf931223@gmail.com>
2021-04-28 22:28:48 +0900

    기본셋팅

---
+++ src/test/java/com/hc/webservice/SpringWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hc.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
